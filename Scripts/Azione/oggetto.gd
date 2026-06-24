4492ea9b28350a31c15eccda887f940a3cab724e
Alberto Di Ronza <albertodironza84@gmail.com>
2026-06-24 10:33:00 +0200

    aabb collision shape dinamica

--- Scripts/Azione/oggetto.gd
+++ Scripts/Azione/oggetto.gd
@@ -1,23 +1,49 @@
 extends RigidBody3D
 
+var mesh_instance: MeshInstance3D = null
+
+@export var model: PackedScene
+var box : BoxShape3D = BoxShape3D.new()
+
 @onready var area_occupata = $"Area occupata"
+var area_box = BoxShape3D.new()
 
 enum State {STORAGE, HAND, PLACED}
 var obj_state: State = State.STORAGE
 var player = null
-# Called when the node enters the scene tree for the first time.
 var half_height: float = 0.0
 
 func _ready() -> void:
+	if model: 
+		var istanza = model.instantiate()
+		#aggiungo scena come figlio del rigidbody così che si muovono assieme
+		add_child(istanza)
+		#Axis aligned bounding box trovo dinamicamente più piccola box che copre l'oggetto
+		var aabb = AABB()
+		var all_children = istanza.find_children("*", "MeshInstance3D", true, false)
+		for child in all_children:
+			if child is MeshInstance3D:
+				mesh_instance = child
+				aabb = mesh_instance.get_aabb()
+				print(aabb.size)
+		
+		box.size = aabb.size * 0.8
+		$CollisionShape3D.shape = box
+		$CollisionShape3D.position = aabb.get_center()
+		
+		area_box.size = aabb.size * 1.5
+		$"Area occupata/CollisionShape3D".shape = area_box
+		$"Area occupata/CollisionShape3D".position = aabb.get_center()
+		
+
 	var shape = $CollisionShape3D.shape as BoxShape3D
 	half_height = shape.size.y / 2.0
 	
 	GameManager.registra_oggetti(self)
-	#se le  metto dall'editor non vengono ereditati da oggetti diversi
+	# se le  metto dall'editor non vengono ereditati da oggetti diversi
 	area_occupata.area_entered.connect(_on_area_occupata_area_entered)
 	area_occupata.area_exited.connect(_on_area_occupata_area_exited)
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
